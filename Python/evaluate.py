d7dcc50eeaad8e532999c9bfbfba604245d80eae
sinamohseni <sina.mohseni89@gmail.com>
2018-01-14 13:46:59 -0600

    Generating user annotation heatmaps and user-weighted masks

--- Python/evaluate.py
+++ Python/evaluate.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*- 
 
+print "importing..."
+
 import re
 import glob
 import copy
@@ -10,6 +12,8 @@ import json
 import csv
 
 import os
+import os.path
+
 
 import codecs
 
@@ -33,103 +37,311 @@ from skimage.segmentation import mark_boundaries
 import scipy.misc
 
 
-def read_data(img_folder, json_file):
 
-    result = []  
-    
+def read_json(json_file):  
+    # Fix if there are multiple annotations per image.
     all_imgs = json.load(open(json_file))
+    images = [];
+    new_user_list = []
+    
+    for an_img in all_imgs:
+        # cut repeated contours
+
+        if an_img["image"] in images:
+            for this_ in new_user_list:
+                if this_["image"] == an_img["image"]:
+                    
+
+                    if ( len(an_img["points"]) > 0):
+                        this_["points"].append(an_img["points"])
+                        # new_user_list.append({"image":an_img["image"],"contour":1,"points": new_points })
+
+                    # del this_
+                    break;
+                    # print "\n ", this_["points"][0], "\n ",this_["points"][1]
+                    
+                    
+
+        else:
+            images.append(an_img["image"]);
+            new_points = [an_img["points"]]
+            new_user_list.append({"image":an_img["image"],"points": new_points }) 
+
+    print "\n number of user annotations", len(new_user_list)#, new_user_list
+    return new_user_list
+
+def read_user_data(img_folder, json_file,tot_user):
+
+    result = []
+    
+    all_imgs = read_json(json_file)    
+
+    imgs_count = len(all_imgs)
 
     for an_img in all_imgs:
 
-        jpg_file = img_folder + an_img["image"];# "test.jpg"; 
+        jpg_file = img_folder + an_img["image"];
         usr_img =  an_img["points"]
 
-        img_exp = img_process(jpg_file,usr_img)
+        img_exp, user_mask = img_process(jpg_file,usr_img,tot_user)
 
-        write_user_exp(img_exp, an_img["image"])
+        # write_user_exp(img_exp, an_img["image"])
+
+        user_mask = mask_exact_obj(user_mask,an_img["image"]);
+
+
+        # cv2.imshow('image',img_exp)   
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
+
+        heatmap_path = './user_mask/'+an_img["image"]+ '.jpg'
+
+        if os.path.exists(heatmap_path): # image_heatmap.size == imgs_count:
+            update_heatmap_mask(heatmap_path, user_mask)
+        else:
+            # print heatmap_path
+            # user_mask[:] = np.arange(255)
+            scipy.misc.imsave(heatmap_path, user_mask)
+
         
         result.append({"image": 0,"recall": 0,"precision": 0,"accuracy": 0})
 
     return result
 
+def mask_exact_obj(user_mask,this_image):
+    
+    # print "This image: ", this_image, len(user_mask), len(ref_mask[0])
+    masked = 0
+    for this_ref in ref_mask:
+        if this_ref[0] == this_image: 
+            user_mask = cv2.bitwise_and(user_mask,this_ref[1])
+            masked = 1
+
+    if (masked == 0):
+        print "\n Error: didn't ref masked: ", this_image
+
+    return user_mask
+
+def reference_mask(img_folder, ref_file,ref_mask):
+
+    # user_mask = read_ref(img_folder, ref_file)   #ref_mask
+
+    all_imgs = read_json(ref_file)    
+
+    imgs_count = len(all_imgs)
+
+    for an_img in all_imgs:
+
+        jpg_file = img_folder + an_img["image"];
+        usr_img =  an_img["points"]
+        # print an_img["image"]
+
+        img_exp, user_mask = img_process(jpg_file,usr_img,1)
+        
+        ref_mask.append( [an_img["image"], user_mask] )      # ref_mask.append(user_mask)    
+
+        # cv2.imshow('image',img_exp)   
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
+
+
+    return 0
+
+def update_heatmap_mask(heatmap_path, user_mask):
+    
+    old_mask = cv2.imread(heatmap_path,0);
+
+    new_mask = cv2.add(old_mask,user_mask)
+
+    # cv2.imshow('image',new_mask)   
+    # cv2.waitKey(0)                
+    # cv2.destroyAllWindows()
+
+    # image_heatmap.append()  
+    scipy.misc.imsave(heatmap_path, new_mask)
+
+    return 0
+
+def user_heatmap(img_folder,heatmap_folder,mask_folder):
+
+    for this_img in ref_mask:
+
+        gray_mask_path = mask_folder + this_img[0] + ".jpg"
+        raw_img_path = img_folder + this_img[0]
+
+        gray_mask = cv2.imread(gray_mask_path,0);
+        raw_img = cv2.imread(raw_img_path,3);
+        
+        heatmap = cv2.applyColorMap(gray_mask,cv2.COLORMAP_JET)  # cv2.COLORMAP_AUTUMN COLORMAP_RAINBOW COLORMAP_JET
+
+        masked_heatmap = cv2.bitwise_and(heatmap,heatmap,mask = this_img[1])
+
+        # print len(raw_img), len(heatmap)
+
+        new_raw_img = raw_img.copy()
+        rows,cols,channels = masked_heatmap.shape
+        roi = new_raw_img[0:rows, 0:cols ]
+        
+        gray_mask_inv = cv2.bitwise_not(gray_mask)
+        # Now black-out the area of logo in ROI
+        img1_bg = cv2.bitwise_and(roi,roi,mask = gray_mask_inv)
+        # Take only region of logo from logo image.
+        img2_fg = cv2.bitwise_and(masked_heatmap,masked_heatmap,mask = gray_mask)
+        # Put logo in ROI and modify the main image
+        dst = cv2.add(img1_bg,img2_fg)
+        # raw_img[0:rows, 0:cols ] = dst
+        
+        new_raw_img[0:rows, 0:cols ] = dst
+        
+        # cv2.imshow('frame0',dst)
+        # cv2.imshow('frame1',raw_img)
+        # cv2.imshow('frame2',new_raw_img)
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
+        # print len(raw_img), len(heatmap)
+
+        alpha = 0.3;
+        blend_heatmap = cv2.addWeighted(raw_img,alpha,new_raw_img,1-alpha,0)
+
+
+        # blend_heatmap = cv2.addWeighted(raw_img,0.6,masked_heatmap,0.6,0)
+        
+        user_heatmap_path = heatmap_folder + this_img[0] + ".jpg"
+
+        # scipy.misc.imsave(user_heatmap_path, blend_heatmap)
+        cv2.imwrite(user_heatmap_path, blend_heatmap)
+        # cv2.imshow('frame',blend_heatmap)
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
+
+
+
+    return 0
+
+def LIME_heatmap(img_folder,LIME_overlay,LIME_mask):
+
+    for this_img in ref_mask:
+
+        gray_mask_path = LIME_mask + "mask_"+ this_img[0] + ".jpg"
+        raw_img_path = img_folder + this_img[0]
+
+        gray_mask = cv2.imread(gray_mask_path,0);
+        raw_img = cv2.imread(raw_img_path,3);
+        raw_img = cv2.resize(raw_img, (299,299))
+
+        heatmap = cv2.applyColorMap(gray_mask,cv2.COLORMAP_JET)  # cv2.COLORMAP_AUTUMN COLORMAP_RAINBOW COLORMAP_JET
+
+        ref_img = cv2.resize(this_img[1], (299,299))
+        masked_heatmap = cv2.bitwise_and(heatmap,heatmap,mask = gray_mask) #this_img[1])
+        # xx = cv2.add(masked_heatmap, raw_img)  # simple add
+        
+        new_raw_img = raw_img.copy()
+        rows,cols,channels = masked_heatmap.shape
+        roi = new_raw_img[0:rows, 0:cols ]
+        
+        gray_mask_inv = cv2.bitwise_not(gray_mask)
+        # Now black-out the area of logo in ROI
+        img1_bg = cv2.bitwise_and(roi,roi,mask = gray_mask_inv)
+        # Take only region of logo from logo image.
+        img2_fg = cv2.bitwise_and(masked_heatmap,masked_heatmap,mask = gray_mask)
+        # Put logo in ROI and modify the main image
+        dst = cv2.add(img1_bg,img2_fg)
+        # raw_img[0:rows, 0:cols ] = dst
+        
+        new_raw_img[0:rows, 0:cols ] = dst
+        
+        # cv2.imshow('frame0',dst)
+        # cv2.imshow('frame1',raw_img)
+        # cv2.imshow('frame2',new_raw_img)
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
+        # print len(raw_img), len(heatmap)
+
+        alpha = 0.4;
+        blend_heatmap = cv2.addWeighted(raw_img,alpha,new_raw_img,1-alpha,0)
+        # cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+        LIME_overlay_path = LIME_overlay + this_img[0] + ".jpg"
+
+        # scipy.misc.imsave(user_heatmap_path, blend_heatmap)
+        cv2.imwrite(LIME_overlay_path, blend_heatmap)
+        # cv2.imshow('frame',blend_heatmap)
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
+
+
+
+    return 0
+
 def write_user_exp(img_exp, jpg_file):
     scipy.misc.imsave('./exp_user/exp_'+jpg_file+ '.jpg', img_exp)
 
-def img_process(jpg_file,usr_img):
+def img_process(jpg_file,usr_img,tot_user):
     
     exp_img = cv2.imread(jpg_file,0)  # load in grayscale 
-    # plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')     # plot show
-    # plt.show()
 
     size_y = exp_img.shape[1] 
     size_x = exp_img.shape[0]     
-    print "image: x ", size_x, "y ", size_y
     
-    blank_image = np.zeros((size_x,size_y,1), np.uint8)
+    mask = np.zeros((size_x,size_y), np.uint8)    #     np.zeros((size_x,size_y,1), np.uint8)
 
-    size_y = blank_image.shape[1] 
-    size_x = blank_image.shape[0]     
-    print "blank: x ", size_x, "y ", size_y
-
-    sketch_size = len(usr_img);
-    # print "sketch_size", sketch_size
-
-    for i in xrange(0,sketch_size):
-        x_ = int(usr_img[i][1])
-        y_ = int(usr_img[i][0])
-        x_ = sorted([0, x_, size_x-1])[1]
-        y_ = sorted([0, y_, size_y-1])[1]
-
-        blank_image[x_,y_] = [255] # ,255,255];
-        exp_img[x_,y_] = 0
+    size_y = mask.shape[1] 
+    size_x = mask.shape[0]
     
+    sketch_points = len(usr_img);
+    # print "sketch_size", sketch_points
 
-    contours = [np.array([usr_img], dtype=np.int32)]
+    contours = []
+
+    for j in xrange(0,sketch_points):
+        sketch_size = len(usr_img[j])
+        for i in xrange(0,sketch_size):
+            x_ = int(usr_img[j][i][1])
+            y_ = int(usr_img[j][i][0])
+            x_ = sorted([0, x_, size_x-1])[1]
+            y_ = sorted([0, y_, size_y-1])[1]
+
+            mask[x_,y_] = 255 / tot_user;
+            exp_img[x_,y_] = 0
+
+        contours.append([np.array([usr_img[j]], dtype=np.int32)])
+        cv2.drawContours(exp_img, [contours[j][0]], 0, (255),3)    # 1: Line thinckness 
+        cv2.drawContours(mask, [contours[j][0]], 0, (255 / tot_user),-1)   # -1: Fill the controur 
     
-    cv2.drawContours(exp_img, [contours[0]], 0, (255),3)    # 1: Line thinckness 
-    cv2.drawContours(blank_image, [contours[0]], 0, (255),-1)   # -1: Fill the controur 
-    
-    # kernel = np.ones((2,2),np.uint8)
-    # dilation = cv2.dilate(blank_image,kernel,iterations = 1)
-    # kernel = np.ones((20,20),np.uint8)
-    # closing = cv2.morphologyEx(blank_image, cv2.MORPH_CLOSE, kernel)
+        # cv2.imshow('image',exp_img)   
+        # cv2.waitKey(0)                
+        # cv2.destroyAllWindows()
 
-    # cv2.imshow('image',exp_img)   
-    # cv2.waitKey(0)                
-    # cv2.destroyAllWindows()
+    return exp_img, mask
 
-    return exp_img
-
-def contour_img(jpg_file):
-    
-    img = cv2.imread(jpg_file,0)  # load in grayscale 
-    
-    # cv2.imshow('image',img)     # img show
-    # cv2.waitKey(0)                
-    # cv2.destroyAllWindows()
-
-    # plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')     # plot show
-    # plt.show()
-
-    ret,thresh = cv2.threshold(img,127,255,0)   
-    im2, contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #(thresh, 1, 2)
-    cnt = contours[0]  
-    print cnt
-    # M = cv2.moments(cnt)
-
-    return cnt
 
 def write_img(jpg_file):
     cv2.imwrite('test.png',img)
 
 def evaluate(img_folder,res_folder):
+    image_heatmap = []
+    user_accuracy = []
 
-    img_number = 1
-    for i in xrange(0,1):   # users loop 
-        contour_img(img_folder + "test.jpg")
-        usr_exp = read_data(img_folder, res_folder+"mohsen.json")
+    tot_user = 9;
+    for i in xrange(1,tot_user+1):   # users loop 
+            # contour_img(img_folder + "test.jpg")
+        print "user: P", i
+        images_accuracy = read_user_data(img_folder, res_folder+"P"+str(i)+".json",tot_user)
+        user_accuracy.append(images_accuracy)
+   
 
+ref_mask = [];
+img_folder = "./raw_img/"
+heatmap_folder = "./user_heatmap/"
+LIME_mask = "./LIME_mask/"
+LIME_overlay = "./LIME_overlay/"
+mask_folder = "./user_mask/"
+res_folder = "./json_res/"
 
-img_folder = "./img/"
-res_folder = "./res/"
-evaluate(img_folder,res_folder)
+# shutil.rmtree(mask_folder)
+# os.remove(mask_folder, *, dir_fd=None)
+print "\n Reference Mask..."
+reference_mask(img_folder, res_folder+"ref.json",ref_mask);
+# print "\n User Mask..."
+# evaluate(img_folder,res_folder)
+user_heatmap(img_folder,heatmap_folder,mask_folder);
+LIME_heatmap(img_folder,LIME_overlay,LIME_mask);
