d7947b0311c05daa1d877f4d6e7a48ae25af7d26
Sina Mohseni <sina.mohseni89@gmail.com>
2020-05-06 20:15:12 -0500

    updating data compiling pipeline and evaluation normalization

--- Python/get_user_attn_map.py
+++ Python/get_user_attn_map.py
@@ -93,7 +93,7 @@ def read_json(json_file):
             new_points = [an_img["points"]]
             new_user_list.append({"image":an_img["image"],"points": new_points }) 
 
-    print ("\n number of user annotations", len(new_user_list))#, new_user_list
+    # print ("User anns: ", len(new_user_list))#, new_user_list
     return new_user_list
 
 def read_user_data(img_folder, json_file,tot_user):
@@ -106,102 +106,45 @@ def read_user_data(img_folder, json_file,tot_user):
 
     for an_img in all_imgs:
 
-        usr_img =  an_img["points"]
-        # jpg_file = img_folder + an_img["image"];
-        jpg_file_1 = img_folder + "cat-"+ an_img["image"] + ".jpg";
-        jpg_file_2 = img_folder + "dog-"+ an_img["image"] + ".jpg";
-        if os.path.exists(jpg_file_1):
-            jpg_file = jpg_file_1
-            heatmap_path = attn_mask_folder + an_img["image"] + ".jpg";  # "cat-" +
-        else: 
-            jpg_file = jpg_file_2
-            heatmap_path = attn_mask_folder + an_img["image"] + ".jpg"; # "dog-" +
-            
+        if an_img["image"] not in attention_checks:
 
-        img_exp, user_mask = img_process(jpg_file,usr_img,tot_user)
+            usr_img =  an_img["points"]
+            jpg_file = img_folder + an_img["image"];
+            heatmap_path = attn_mask_folder + an_img["image"];
 
-        # write_user_exp(img_exp, an_img["image"])
+            img_exp, user_mask = img_process(jpg_file,usr_img,tot_user)
 
-        user_mask = mask_exact_obj(user_mask,an_img["image"]);
+            user_mask = mask_exact_obj(user_mask,an_img["image"]);
 
+            # cv2.imshow('image',img_exp)   
+            # cv2.waitKey(0)                
+            # cv2.destroyAllWindows()
 
-        # cv2.imshow('image',img_exp)   
-        # cv2.waitKey(0)                
-        # cv2.destroyAllWindows()
+            if os.path.exists(heatmap_path): # image_heatmap.size == imgs_count:
+                update_heatmap_mask(heatmap_path, user_mask)
+            else:
+                # print (heatmap_path)
+                cv2.imwrite(heatmap_path, user_mask)
 
-        # heatmap_path = mask_folder+an_img["image"]  # './Image/user_mask/'
-
-        if os.path.exists(heatmap_path): # image_heatmap.size == imgs_count:
-            update_heatmap_mask(heatmap_path, user_mask)
-        else:
-            print (heatmap_path)
-            cv2.imwrite(heatmap_path, user_mask)
-
-        
-        result.append({"image": 0,"recall": 0,"precision": 0,"accuracy": 0})
+            result.append({"image": 0,"recall": 0,"precision": 0,"accuracy": 0})
 
     return result
 
 
-
 def segmentation_mask(ref_mask, seg_mask_folder):
 
+    print ('segmentation_mask...')
     result = []
     
-    # all_imgs = read_json(json_file)    
-
-    # imgs_count = len(all_imgs)
     for this_ref in ref_mask:
-        # usr_img =  an_img["points"]
-        # if os.path.exists(jpg_file_1):
-        #     seg_mask_path = seg_mask_folder + "cat-" + this_ref[0] + ".jpg";
-        # else: 
-        #     seg_mask_path = seg_mask_folder + "dog-" + this_ref[0] + ".jpg";
-
-        seg_mask_path = seg_mask_folder + this_ref[0] + ".jpg";
-        
-        if os.path.exists(seg_mask_path): # image_heatmap.size == imgs_count:
-            print ('already axists! ', seg_mask_path)
-            # update_heatmap_mask(heatmap_path, user_mask)
-        else:
-            print (seg_mask_path)
-            cv2.imwrite(seg_mask_path, this_ref[1])
-
-    # for an_img in all_imgs:
-
-
-
-    #     usr_img =  an_img["points"]
-    #     if os.path.exists(jpg_file_1):
-    #         jpg_file = jpg_file_1
-    #         heatmap_path = seg_mask_folder + "cat-" + an_img["image"] + ".jpg";
-    #     else: 
-    #         jpg_file = jpg_file_2
-    #         heatmap_path = seg_mask_folder + "dog-" + an_img["image"] + ".jpg";
+        if this_ref[0] not in attention_checks:
+            seg_mask_path = seg_mask_folder + this_ref[0];
             
-
-    #     img_exp, user_mask = img_process(jpg_file,usr_img,tot_user)
-
-    #     # write_user_exp(img_exp, an_img["image"])
-
-    #     user_mask = mask_exact_obj(user_mask,an_img["image"]);
-
-
-    #     # cv2.imshow('image',img_exp)   
-    #     # cv2.waitKey(0)                
-    #     # cv2.destroyAllWindows()
-
-    #     # heatmap_path = mask_folder+an_img["image"]  # './Image/user_mask/'
-
-    #     if os.path.exists(heatmap_path): # image_heatmap.size == imgs_count:
-    #         update_heatmap_mask(heatmap_path, user_mask)
-    #     else:
-    #         print (heatmap_path)
-
-    #         cv2.imwrite(heatmap_path, user_mask)
-
-        
-    #     result.append({"image": 0,"recall": 0,"precision": 0,"accuracy": 0})
+            if os.path.exists(seg_mask_path): # image_heatmap.size == imgs_count:
+                print ('already exists! ', seg_mask_path)
+                # update_heatmap_mask(heatmap_path, user_mask)
+            else:
+                cv2.imwrite(seg_mask_path, this_ref[1])  #   *annt_per_imgs
 
     return 0
 
@@ -215,39 +158,33 @@ def mask_exact_obj(user_mask,this_image):
             masked = 1
 
     if (masked == 0):
-        print ("\n Error: couldn't get the ref masked: ", this_image)
+        print ("\n Error: couldn't find the ref masked: ", this_image)
 
     return user_mask
 
+
+
 def reference_mask(img_folder, ref_file,ref_mask):
-    print ("\n Reference Mask...")
+    print (" Reference Mask...")
     # user_mask = read_ref(img_folder, ref_file)   #ref_mask
 
     all_imgs = read_json(ref_file)    
 
-    imgs_count = len(all_imgs)
-
     for an_img in all_imgs:
 
-        jpg_file_1 = img_folder + "cat-"+ an_img["image"] + ".jpg";
-        jpg_file_2 = img_folder + "dog-"+ an_img["image"] + ".jpg";
-        if os.path.exists(jpg_file_1):
-            jpg_file = jpg_file_1
-        else: 
-            jpg_file = jpg_file_2
+        if an_img["image"] not in attention_checks:
+            jpg_file = img_folder +  an_img["image"];       # + "cat-"+ an_img["image"] + ".jpg";
 
-        usr_img =  an_img["points"]
-        # print an_img["image"]
+            usr_img =  an_img["points"];
 
-        img_exp, user_mask = img_process(jpg_file,usr_img,1)
-        
-        ref_mask.append( [an_img["image"], user_mask] )      # ref_mask.append(user_mask)    
-        # ref_mask.append( [jpg_file, user_mask] )      # ref_mask.append(user_mask)    
-
-        # cv2.imshow('image',img_exp)   
-        # cv2.waitKey(0)                
-        # cv2.destroyAllWindows()
-
+            img_exp, user_mask = img_process(jpg_file,usr_img,1)
+            
+            ref_mask.append( [an_img["image"], user_mask] ) 
+        # else:
+        #     print ("Attention Check: ", an_img["image"])
+            # cv2.imshow('image',img_exp)   
+            # cv2.waitKey(0)                
+            # cv2.destroyAllWindows()
 
     return 0
 
@@ -271,59 +208,51 @@ def user_heatmap(img_folder,heatmap_folder,attn_mask_folder):
     print ("\n User heatmap...")
     for this_img in ref_mask:
 
-        # gray_mask_path = attn_mask_folder + this_img[0]
-        # raw_img_path = img_folder + this_img[0]
+        if this_img[0] not in attention_checks:
+            gray_mask_path = attn_mask_folder + this_img[0]
+            raw_img_path = img_folder + this_img[0]
+            
+            gray_mask = cv2.imread(gray_mask_path,0);
+            raw_img = cv2.imread(raw_img_path,3);
+            
+            heatmap = cv2.applyColorMap(gray_mask,cv2.COLORMAP_TURBO)  # COLORMAP_JET COLORMAP_TURBO cv2.COLORMAP_AUTUMN COLORMAP_RAINBOW 
+            masked_heatmap = cv2.bitwise_and(heatmap,heatmap,mask = this_img[1])
 
-        gray_mask_path_1 = attn_mask_folder + "cat-"+ this_img[0] + ".jpg";
-        gray_mask_path_2 = attn_mask_folder + "dog-"+ this_img[0] + ".jpg";
-        if os.path.exists(gray_mask_path_1):
-            gray_mask_path = gray_mask_path_1
-            raw_img_path = img_folder + "cat-"+ this_img[0] + ".jpg";
-        else: 
-            gray_mask_path = gray_mask_path_2
-            raw_img_path = img_folder + "dog-"+ this_img[0] + ".jpg";
+            new_raw_img = raw_img.copy()
+            rows,cols,channels = masked_heatmap.shape
+            roi = new_raw_img[0:rows, 0:cols ]
+            
+            gray_mask_inv = cv2.bitwise_not(gray_mask)
+            # Now black-out the area of logo in ROI
+            img1_bg = cv2.bitwise_and(roi,roi,mask = gray_mask_inv)
+            # Take only region of logo from logo image.
+            img2_fg = cv2.bitwise_and(masked_heatmap,masked_heatmap,mask = gray_mask)
+            # Put logo in ROI and modify the main image
+            dst = cv2.add(img1_bg,img2_fg)
+            # raw_img[0:rows, 0:cols ] = dst
+            
+            new_raw_img[0:rows, 0:cols ] = dst
+            
+            # cv2.imshow('frame0',dst)
+            # cv2.imshow('frame1',raw_img)
+            # cv2.imshow('frame2',new_raw_img)
+            # cv2.waitKey(0)                
+            # cv2.destroyAllWindows()
+            # print len(raw_img), len(heatmap)
 
-        gray_mask = cv2.imread(gray_mask_path,0);
-        raw_img = cv2.imread(raw_img_path,3);
-        
-        heatmap = cv2.applyColorMap(gray_mask,cv2.COLORMAP_JET)  # COLORMAP_TURBO  cv2.COLORMAP_AUTUMN COLORMAP_RAINBOW COLORMAP_JET
-        masked_heatmap = cv2.bitwise_and(heatmap,heatmap,mask = this_img[1])
-
-        new_raw_img = raw_img.copy()
-        rows,cols,channels = masked_heatmap.shape
-        roi = new_raw_img[0:rows, 0:cols ]
-        
-        gray_mask_inv = cv2.bitwise_not(gray_mask)
-        # Now black-out the area of logo in ROI
-        img1_bg = cv2.bitwise_and(roi,roi,mask = gray_mask_inv)
-        # Take only region of logo from logo image.
-        img2_fg = cv2.bitwise_and(masked_heatmap,masked_heatmap,mask = gray_mask)
-        # Put logo in ROI and modify the main image
-        dst = cv2.add(img1_bg,img2_fg)
-        # raw_img[0:rows, 0:cols ] = dst
-        
-        new_raw_img[0:rows, 0:cols ] = dst
-        
-        # cv2.imshow('frame0',dst)
-        # cv2.imshow('frame1',raw_img)
-        # cv2.imshow('frame2',new_raw_img)
-        # cv2.waitKey(0)                
-        # cv2.destroyAllWindows()
-        # print len(raw_img), len(heatmap)
-
-        alpha = 0.5;
-        blend_heatmap = cv2.addWeighted(raw_img,alpha,new_raw_img,1-alpha,0)
+            alpha = 0.5;
+            blend_heatmap = cv2.addWeighted(raw_img,alpha,new_raw_img,1-alpha,0)
 
 
-        # blend_heatmap = cv2.addWeighted(raw_img,0.6,masked_heatmap,0.6,0)
-        
-        user_heatmap_path = heatmap_folder + this_img[0].split(".")[0] + ".png"
+            # blend_heatmap = cv2.addWeighted(raw_img,0.6,masked_heatmap,0.6,0)
+            
+            user_heatmap_path = heatmap_folder + this_img[0].split(".")[0] + ".png"
 
-        # scipy.misc.imsave(user_heatmap_path, blend_heatmap)
-        cv2.imwrite(user_heatmap_path, blend_heatmap)  # 
-        # cv2.imshow('frame',blend_heatmap)
-        # cv2.waitKey(0)                
-        # cv2.destroyAllWindows()
+            # scipy.misc.imsave(user_heatmap_path, blend_heatmap)
+            cv2.imwrite(user_heatmap_path, blend_heatmap)  # 
+            # cv2.imshow('frame',blend_heatmap)
+            # cv2.waitKey(0)                
+            # cv2.destroyAllWindows()
 
 
 
@@ -390,10 +319,10 @@ def write_user_exp(img_exp, jpg_file):
 
 def img_process(jpg_file,usr_img,tot_user):
 
-    exp_img = cv2.imread(jpg_file,0)  # load in grayscale 
+    exp_img = cv2.imread(jpg_file,0)    # load in grayscale 
 
-    size_y = exp_img.shape[1] 
-    size_x = exp_img.shape[0]     
+    size_y = exp_img.shape[1]
+    size_x = exp_img.shape[0]   
     
     mask = np.zeros((size_x,size_y), np.uint8)    #     np.zeros((size_x,size_y,1), np.uint8)
 
@@ -410,20 +339,20 @@ def img_process(jpg_file,usr_img,tot_user):
         new_usr_img = []
         for i in range(0,sketch_size):
 
-            x_ = int(float(usr_img[j][i][1]))
-            y_ = int(float(usr_img[j][i][0]))
+            x_ = int(float(usr_img[j][i][1])*img_to_ann_ratio)
+            y_ = int(float(usr_img[j][i][0])*img_to_ann_ratio)
             x_ = sorted([0, x_, size_x-1])[1]
             y_ = sorted([0, y_, size_y-1])[1]
 
-            new_usr_img.append([float(usr_img[j][i][0]),float(usr_img[j][i][1])])
+            new_usr_img.append([float(usr_img[j][i][0])*img_to_ann_ratio,float(usr_img[j][i][1])*img_to_ann_ratio])
 
-            mask[x_,y_] = 255 / tot_user;
+            mask[x_,y_] = 255 / annt_per_imgs;
             exp_img[x_,y_] = 0
 
         contours.append([np.array([new_usr_img], dtype=np.int32)])
         # contours.append([np.array([_usr_img[j]], dtype=np.int32)])
         cv2.drawContours(exp_img, [contours[j][0]], 0, (255),3)    # 1: Line thinckness 
-        cv2.drawContours(mask, [contours[j][0]], 0, (255 / tot_user),-1)   # -1: Fill the controur 
+        cv2.drawContours(mask, [contours[j][0]], 0, (255 / annt_per_imgs),-1)   # -1: Fill the controur 
     
         # cv2.imshow('image',exp_img)   
         # cv2.waitKey(0)                
@@ -434,32 +363,35 @@ def img_process(jpg_file,usr_img,tot_user):
 
 def user_att_mask(img_folder,res_folder):
     print ("\n User Mask...")
-    image_heatmap = []
-    user_accuracy = []
 
-    
+
     for i in range(1,tot_user+1):   # users loop 
-            # contour_img(img_folder + "test.jpg")
-        print ("user: P", i)
+
+        print ("User: P", i)
         images_accuracy = read_user_data(img_folder, res_folder+"P"+str(i)+".json",tot_user)
-        user_accuracy.append(images_accuracy)
    
 
-batches = ['batch-1'] # ,'batch-2','batch-3','batch-4']
+batches = ['batch-2'] # ,'batch-2','batch-3','batch-4']
+tot_user = 91;
+annt_per_imgs = 9;
+img_to_ann_ratio = 0.5;
 
 for batch in batches:
 
     print ('Batch: ', batch)
 
+    # 0- Create participants' annotation records from mturk csv
     # get_jsons(batch)
+    # get_workers(batchs)
 
-    tot_user = 10;
+    
     ref_mask = [];
     img_folder = "../data/VOC2012_raw/"                     # original raw images  
     attn_mask_folder = "../data/user_attn_maps/"+batch+"/"              # Final binary human-attention mask
     seg_mask_folder = "../data/user_seg_maps/"+batch+"/"              # Final binary human-attention mask
     
     res_folder = "../user-study/mturk-annotation-results/"+batch+"/json/"
+    ref_folder = "../user-study/mturk-annotation-results/"+batch+"/ref/"
 
     # to save an overlay of user attention-map on raw image
     heatmap_folder = "../data/user_attn_overlay/"+batch+"/"
@@ -467,19 +399,23 @@ for batch in batches:
     LIME_mask = "./Image/LIME_mask/"
     LIME_overlay = "./Image/LIME_overlay/"
 
-    test_folder = "../data/test/"  
-    files_name(test_folder)
+    attention_checks = ["cat-2007_000528.jpg","cat-2007_000876.jpg","cat-2007_003778.jpg","cat-2008_000824.jpg"]
+
+    # test_folder = "../data/test/"  
+    # files_name(test_folder)
     
     # Genrating objects reference mask useing ref contur
-    # reference_mask(img_folder, res_folder+"ref.json",ref_mask);
+    keys = [0,1,2,3,4,5,6,7,8,9]
+    for each_key in keys:
+        reference_mask(img_folder, ref_folder+"ref-"+str(each_key)+".json",ref_mask);
 
     # 1- Generating users weighted mask with user data
     # erase_folder(attn_mask_folder)
     # user_att_mask(img_folder,res_folder)
 
     # 2- Generating users weighted mask with user data
-    # erase_folder(seg_mask_folder)
-    # segmentation_mask(ref_mask,seg_mask_folder)
+    erase_folder(seg_mask_folder)
+    segmentation_mask(ref_mask,seg_mask_folder)
 
     # 3- Generating user heatmaps for visualization
     # erase_folder(heatmap_folder)
--- Python/mturk_to_user_rating.py
+++ Python/mturk_to_user_rating.py
@@ -22,41 +22,93 @@ import pandas as pd
 
 
 
+def dic_normalization(this_dic): 
+
+    min_value = this_dic.pop('min')
+    max_value = this_dic.pop('max')
+    range_value = max_value - min_value; 
+    
+    # print (this_dic['max'], this_dic['min'])
+    # print (range_value)
+
+    for each in this_dic:
+        this_dic[each] = round(((this_dic[each] - min_value) / range_value),3);
+
 
 def get_ratings(batch):
 	
-	in_folder = "../user-study/mturk-rating-results/"+batch+"/1.csv";
-	out_folder = "../user-study/mturk-rating-results/"+batch+"/json/";
+	max_rate = -100; min_rate = 100;
 
+	in_folder = "../user-study/mturk-rating-results/"+batch+"/mturk.csv";
+	out_folder = "../user-study/mturk-rating-results/"+batch+"/json/";
 
 	entire_log = pd.read_csv(in_folder, doublequote=True, escapechar='\\')  # 
 	results = entire_log['Answer.surveycode']
 	images = {}  # 	{image_xx: [ rating1, rating2,..., rating_n]}
 	
 	i=0
+	# each participant
 	for each in results: 
-		i+=1;
-		each_json = json.loads(each)
-		each_json.pop(0)			# pop out the mtruk id record
-		# print (each_json.pop(0))  # pop out the mtruk id record
-		for single in each_json:
-			# print ("single", single)
-			if single['i'] in images: 
-				images[single['i']].append(int(single['r']))
-			else: 
-				images[single['i']] = [int(single['r'])]
+		try:
+			print ('P: ',i)
+			each_json = json.loads(each)
+			try:
+				i+=1;
+				each_json.pop(0);			# pop out the mtruk id record
+				
+				# each image rating
+				# >>>>>>>>>>>> Normalize each participants rating here >>>>>>>>>>>>>>>
+				max_r = -100; min_r = 100;  
+				for single in each_json: 
+					if single['i'] not in attention_checks:
+						this_rating = int(single['r'])
+						if (this_rating > max_r): max_r = this_rating;
+						if (this_rating < min_r): min_r = this_rating;
+
+				range_value = max_r - min_r;
+				for single in each_json:
+					if single['i'] not in attention_checks:
+						# >>>>>>>>>>>> Without Normalization >>>>>>>>>>>>>>>
+						# with out normalization
+						this_rating = int(single['r'])   
+						# with the normalization
+						# this_rating = round(((this_rating - min_r) / range_value),3)
+
+						if single['i'] in images: 
+							images[single['i']].append(this_rating)
+						else: 
+							images[single['i']] = [this_rating]
+			except:
+				print ('Error: ', each_json)    # pop out the mtruk id record
+		except:
+			print ('Error: ', each)    # pop out the mtruk id record
 
 
-	for each in images: 
-		# print ('images: ',each, images[each])
-		images[each] = round((sum(images[each])/len(images[each]))/10, 3)
-		# print ('images: ',each, images[each])
+	take_it_out = []
+	for each in images:
+		if (len(images[each]) > 3):
+			# >>>> No normalization   >>> 
+			# this_rating = round((sum(images[each])/len(images[each]))/10, 3)
+			
+			# >>>> With normalization   >>> 
+			this_rating = sum(images[each])/len(images[each])
+			
+			print ('images: ',each, images[each])
+			images[each] = this_rating;
+			if (this_rating > max_rate): max_rate = this_rating;
+			if (this_rating < min_rate): min_rate = this_rating;
+		else: 
+			take_it_out.append(each);	
 
-		# with open(out_folder+'P'+str(i)+'.json','w', encoding='utf-8') as f:
-		# 	json.dump(each_json, f, ensure_ascii=False)
+	for each in take_it_out:
+		print('less than 3 ratings: ',images.pop(each))
+
+	# >>>>>>>>>>>> Normalize all images ratings here >>>>>>>>>>>>> 
+	images["max"] = max_rate
+	images["min"] = min_rate
+	dic_normalization(images)
 
 	return images;
-
 
 
 def get_workers(batch):
@@ -96,7 +148,10 @@ def get_workers(batch):
 	return all_workers;
 
 
-batchs = 'batch-1';
-get_workers(batchs)
+attention_checks = ["cat-2007_000528.jpg","cat-2007_000876.jpg","cat-2007_003778.jpg","cat-2008_000824.jpg"]
+
+
+# batchs = 'batch-1';
+# get_workers(batchs)
 
 
