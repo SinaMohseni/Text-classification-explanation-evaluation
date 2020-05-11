c352fc10e41f46d04161f1a7a28521dd0c772d1f
Jeremy Block <j.block@ufl.edu>
2020-05-11 12:02:33 -0400

    Added Timer handleing and pushing all to the json.php file.

--- benchmark/scripts/procedureController.js
+++ benchmark/scripts/procedureController.js
@@ -1,16 +1,19 @@
-let start_time = Math.floor(Date.now() / 1000);
-let last_time_s = start_time;
-function Progress(main_element,data,callBackMethod){
+function Progress(main_element,data,callBackMethod, condition){
+    this.progress_start_time = Math.floor(Date.now() / 1000);
+    this.last_time_s = this.progress_start_time;
     this.i = 0 // Current page to resolve/look at
     this.total = data.length //Total number of pages
     this.main_element = main_element //What element needs to change on each page.
     this.d = data; //The content to be displayed on screen
     this.callBackMethod = callBackMethod //Where to go after the sequence of progress.
+    this.conditionNum = condition //a number 0-3 describing the kind of dataset the users is seeing.
     this.userData = new Array(this.total)// not sure if I need this saved here yet...
     this.hasSeen = new Array(this.total) //list of booleans if they have been seen.
+    this.timeOnPage = new Array(this.total) //How long have they been looking at this trial/page
     for (let index = 0; index < this.total; index++) { //filling in the list of seen as false at first.
         this.hasSeen[index] = false;
         this.userData[index] = new Array();
+        this.timeOnPage[index] = 0;
         console.log("filling array's with default values")
     }
     this.saw = function (index){
@@ -20,22 +23,36 @@ function Progress(main_element,data,callBackMethod){
         resolveProgressButtons();
         return true;//unnecessary but just in case.
     }
+    this.unsaw = function(index){
+        if(typeof index == "undefined"){index = this.i} //use index provided or just mark the current index as seen
+        this.hasSeen[index]=false;
+        resolveProgressButtons();
+        return false;//unnecessary but just in case.       
+    }
     // overwrites data stored in an index
     this.saveData = function(updatedObj,shouldOverwrite){
-        current_time_s = Math.floor(Date.now() / 1000);
-        tot_time = current_time_s - last_time_s;
-        last_time_s = current_time_s;
-        console.log(tot_time)
+        let current_time_s = Math.floor(Date.now() / 1000);
+        let tot_time = current_time_s - this.last_time_s;
+        this.last_time_s = current_time_s;
+        // console.log("time on page: ", tot_time, "(s), currently stored:", updatedObj["secSinceLast"]);
         updatedObj["secSinceLast"]=tot_time;
+        this.timeOnPage[this.i] += tot_time;
+        // if (this.userData[this.i].tot_time == undefined){
+        //     updatedObj["tot_time"] = tot_time
+        // } else {
+        //     updatedObj["tot_time"] = this.userData[this.i].tot_time +tot_time
+        // }
         this.saw(this.i)
         // console.log("you want to add:", updatedObj, "to", this.userData);
+        //todo When clearing an object, it doesn't clear away the "secSinceLast" property of the empty array...
 
         if(shouldOverwrite){ //overwrite the data
             this.userData.splice(this.i,1,updatedObj);
         } else{ //append to the data in this index
             this.userData[this.i].push(updatedObj)
         }
-        // console.log("Did that work? Here's the Data I have now",this.userData);
+            //    console.log(this.userData[this.i]);
+ console.log("Did that work? Here's the Data I have now",this.userData);
     }
     this.writeToFile = function(taskNum){
         let toSave = []; //final output array to be built now and saved
@@ -45,29 +62,30 @@ function Progress(main_element,data,callBackMethod){
         let dataset_key = task_key_id.split(",")[1]; //separate out the dataset key so we know what they observed
         let mturk_id = task_key_id.split(",")[2]; //separate their MTurk ID so we know who they are.
         
-        //todo check that timers are working and saving is happening correctly.
         //Calculate the Total Time the Task took to complete
         let task_end_time = Math.floor(Date.now() / 1000);
-        let task_total_time = task_end_time - task_start_time;
+        let task_total_time = task_end_time - this.progress_start_time;
 
         //first entry contrins all this information
         toSave.push({i: mturk_id, r:dataset_key, t:taskNum, d:0,d1:tutorial_time,d2:task_total_time});
 
-        //push the remainder of the user data to this file.
-        for (let index = 0; index < this.userData.length; index++) {
-            toSave.push(this.userData[index]);
+        for (let index = 0; index < this.total; index++) {
+            this.userData[index].unshift({PageTime:this.timeOnPage[index]})
+            console.log(this.userData[index])
+            // toSave.push(this.userData[index]);
         }
-
+        //push the remainder of the user data to this file.
+        toSave.push(this.userData);
         //now Save the file as json to the server with a POST request.
         $.ajax({
             type : "POST",
-            url : "json.php",
+            url : "/benchmark/json.php",
             data : {
                 json : JSON.stringify(toSave)
             }
           });
           //Call the Callback function final page after being written.
-        //   this.callBackMethod();
+          this.callBackMethod();
     }
     this.back = function (){
         // console.log("called Back to page #", this.i, "out of", this.total, "user has seen:",this.hasSeen)
@@ -85,8 +103,8 @@ function Progress(main_element,data,callBackMethod){
         // }else if(this.i == this.total-2){
         //     document.getElementById("nextbutton-1").innerHTML = "Start Task"
         }else{
-            this.writeToFile();
-            progressFinished()
+            this.writeToFile(this.conditionNum);
+            // this.callBackMethod()
         }
     }
     this.updatePage = function(){
@@ -129,3 +147,24 @@ function resolveProgressButtons(){
 		document.getElementById("backbutton-2").disabled = false;
 	}
 }
+
+//Adding generic Getters and Setters for Cookies so the don't need to be added to every html document in addition ot this object.
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+
+  }
+
+  function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+  }
